fc02ec896f7b4a0f4c10507437be65984bc4525a
David Festal <dfestal@redhat.com>
2026-04-13 23:03:01 +0200

    Decouple publishing of the main crate and the nodejs bindings
    Signed-off-by: David Festal <dfestal@redhat.com>

--- bindings/nodejs/scripts/sync-version.js
+++ bindings/nodejs/scripts/sync-version.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const args = process.argv.slice(2);
+const checkOnly = args.includes("--check");
+const tagIdx = args.indexOf("--tag");
+const tag = tagIdx !== -1 ? args[tagIdx + 1]?.replace(/^v/, "") : null;
 
 const cargo = fs
   .readFileSync("../../Cargo.toml", "utf8")
@@ -12,10 +16,23 @@ if (!cargo) {
 const pkgPath = "package.json";
 const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf8"));
 
-if (pkg.version !== cargo) {
-  pkg.version = cargo;
-  fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + "\n");
-  console.log("Synced package.json version to", cargo);
-} else {
-  console.log("Versions already in sync:", cargo);
+console.log(`Cargo.toml: ${cargo} | package.json: ${pkg.version}${tag ? ` | tag: ${tag}` : ""}`);
+
+let failed = false;
+if (tag && cargo !== tag) {
+  console.error(`::error::Cargo.toml version (${cargo}) does not match git tag (${tag})`);
+  failed = true;
 }
+if (pkg.version !== cargo) {
+  if (checkOnly) {
+    console.error(`::error::package.json version (${pkg.version}) does not match Cargo.toml (${cargo})`);
+    failed = true;
+  } else {
+    pkg.version = cargo;
+    fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + "\n");
+    console.log("Synced package.json version to", cargo);
+  }
+} else {
+  console.log("Versions in sync:", cargo);
+}
+if (failed) process.exit(1);
